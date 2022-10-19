6ea66332bf175648a43db691a6e59e9957ab1631
jump3rhood <mjm13121@gmail.com>
2022-10-19 13:24:27 +0530

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,5 +9,13 @@ const dummy = ( blogs ) => {
 const totalLikes = (blogs) => {
     return blogs.reduce( (a,b) => a + b.likes , 0)
 }
-
-module.exports = { dummy, totalLikes }
+// takes a list of blogs and returns the one with most likes
+const favoriteBlog = (blogs) => {
+    let maxLikes = blogs[0].likes
+    for(let blog of blogs){
+        if( blog.likes > maxLikes )
+            maxLikes = blog.likes
+    }
+    return blogs.find( blog => blog.likes === maxLikes )
+}
+module.exports = { dummy, totalLikes, favoriteBlog }
