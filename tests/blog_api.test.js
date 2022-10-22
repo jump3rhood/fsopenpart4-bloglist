3314faa9d9b44f0a7417c776e9d7c96982e1b3fa
jump3rhood <mjm13121@gmail.com>
2022-10-22 15:26:53 +0530

    4.11*: Blog list tests, step4

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -44,6 +44,20 @@ test('a valid blog can be added', async () => {
   expect(allBlogs).toHaveLength(helper.blogs.length + 1)
 })
 
+test('a blog without likes property gets 0 as default value', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  }
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+  const addedBlog = response.body
+  expect(addedBlog.likes).toBe(0)
+
+})
 afterAll(() => {
   mongoose.connection.close()
 })
